4fb60339804a70b4028f2df11a5092b231daf4b1
Daniel Nuñez <darkakish@gmail.com>
2022-05-05 21:10:34 -0400

    Added user auth

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -1,25 +1,40 @@
 import { Request, Response } from 'express'
 import { CreateTaskDTO, TaskDTO, UpdateTaskDTO } from '../models/dto/TaskDTO'
+import { UserTokenPayload } from '../models/dto/UserDTO'
 import TaskRepository from '../models/repositories/TaskRepository'
 import { createTaskSchema, updateTaskSchema } from '../models/validators/taskSchemas'
 
 export default class TaskController {
-  public readonly getAll = async (_req: Request, res: Response) => {
-    const repository = new TaskRepository(1)
-    const tasks: TaskDTO[] = await repository.findAll()
-    res.json(tasks)
+  public readonly getAll = async (req: Request, res: Response) => {
+    const user = req.user as UserTokenPayload
+    const repository = new TaskRepository(user.sub)
+    try {
+      const tasks: TaskDTO[] = await repository.findAll()
+      res.json(tasks)
+    } catch (error) {
+      console.log(error);
+      console.log('Error.code', error.code);
+      res.status(500).json({ message: 'Something went wrong' })
+    }
   }
 
   public readonly getById = async (req: Request, res: Response) => {
+    const user = req.user as UserTokenPayload
     const { id } = req.params
-    const repository = new TaskRepository(1)
+    const repository = new TaskRepository(user.sub)
     const task = await repository.findById(parseInt(id))
 
-    if (!task) {
-      res.status(404).json({ message: 'Task not found' })
-      return
+    try {
+      if (!task) {
+        res.status(404).json({ message: 'Task not found' })
+        return
+      }
+      res.json(task)
+    } catch (error) {
+      console.log(error);
+      console.log('Error.code', error.code);
+      res.status(500).json({ message: 'Something went wrong' })
     }
-    res.json(task)
   }
 
   public readonly create = async (req: Request, res: Response) => {
@@ -32,9 +47,21 @@ export default class TaskController {
       return
     }
 
-    const repository = new TaskRepository(1)
-    const newTask = await repository.create(task)
-    res.json(newTask)
+    const user = req.user as UserTokenPayload
+    const repository = new TaskRepository(user.sub)
+
+    try {
+      const newTask = await repository.create(task)
+      res.json(newTask)
+    } catch (error) {
+      if (error.code === 'P2002') {
+        res.status(409).json({ message: 'Task already exists' })
+        return
+      }
+      console.log(error);
+      console.log('Error.code', error.code);
+      res.status(500).json({ message: 'Something went wrong' })
+    }
   }
 
   public readonly update = async (req: Request, res: Response) => {
@@ -48,18 +75,34 @@ export default class TaskController {
       return
     }
 
-    const repository = new TaskRepository(1)
-
-    await repository.update(parseInt(id), task)
-    res.sendStatus(204)
+    const user = req.user as UserTokenPayload
+    const repository = new TaskRepository(user.sub)
+    try {
+      await repository.update(parseInt(id), task)
+      res.sendStatus(204)
+    } catch (error) {
+      if (error.code === 'P2002') {
+        res.status(409).json({ message: 'Task already exists' })
+        return
+      }
+      console.log(error);
+      console.log('Error.code', error.code);
+      res.status(500).json({ message: 'Something went wrong' })
+    }
   }
 
   public readonly delete = async (req: Request, res: Response) => {
     const { id } = req.params
+    const user = req.user as UserTokenPayload
+    const repository = new TaskRepository(user.sub)
+    try {
+      await repository.delete(parseInt(id))
+      res.sendStatus(204)
 
-    const repository = new TaskRepository(1)
-
-    await repository.delete(parseInt(id))
-    res.sendStatus(204)
+    } catch (error) {
+      console.log(error);
+      console.log('Error.code', error.code);
+      res.status(500).json({ message: 'Something went wrong' })
+    }
   }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import taskRoutes from './taskRoutes';
 import healthRoutes from './healthRoutes';
+import authRoutes from './authRoutes';
+import tokenValidator from '../middlewares/tokenValidator';
 
 const apiRoutes = Router();
 
 apiRoutes.use('/', healthRoutes)
-apiRoutes.use('/tasks', taskRoutes)
+apiRoutes.use('/tasks', tokenValidator(), taskRoutes)
+apiRoutes.use('/auth', authRoutes)
 
 export default apiRoutes 
